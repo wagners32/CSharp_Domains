313889ad17535a7424529f083ac74278872db9e9
wagners32 <wagnererlike@gmail.com>
2021-07-20 08:47:36 -0300

    Revert "Add domain test for title"
    This reverts commit b708ab630e072279d148c296d73153ca70c64c75.

--- BookContext/BookContext.Domain/Book.cs
+++ BookContext/BookContext.Domain/Book.cs
@@ -1,5 +1,3 @@
-using BookContext.Domain.Exceptions;
-
 namespace BookContext.Domain
 {
     public class Book
@@ -10,24 +8,9 @@ namespace BookContext.Domain
 
         public Book(string title, string author, int year)
         {
-            AtribuiDadosDoLivro(title, author, year);
-        }
-
-        private void AtribuiDadosDoLivro(string title, string author, int year) {
-            ValidaDadosDoLivro(title, author, year);
             Title = title;
             Author = author;
             Year = year;       
-        }
-
-        private static void ValidaDadosDoLivro(string title, string author, int year) {
-            VerificaSePossuiTitulo(title);
-
-        }
-
-        private static void VerificaSePossuiTitulo(string title) {
-            if (string.IsNullOrWhiteSpace( title))
-             throw new DomainException("Título do livro é uma informação obrigatória.");
         }
     }
 }
--- BookContext/BookContext.Tests/Domain/BookTest.cs
+++ BookContext/BookContext.Tests/Domain/BookTest.cs
@@ -1,7 +1,6 @@
 using System;
 using Xunit;
 using BookContext.Domain;
-using BookContext.Domain.Exceptions;
 
 namespace BookContext.Tests.Domain
 {
@@ -11,22 +10,12 @@ namespace BookContext.Tests.Domain
         public void Book_QuandoCriado_DeveAtribuirDados()
         {
             // given
-            var book = new Book(title: "Livro Um", author: "João Maria", year: 2021);
+            var book = new Book(title: "Teste", author: "Wagner", year: 2021);
 
             // assert
-            Assert.True(book.Title == "Livro Um");
-            Assert.True(book.Author == "João Maria");
+            Assert.True(book.Title == "Teste");
+            Assert.True(book.Author == "Wagner");
             Assert.True(book.Year == 2021);
         }
-        
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        public void QuandoTitleNuloOuVazio_DeveLancarDomainException(string title)
-        {
-            Assert.Throws<DomainException>(() => new Book(title: title, author: "João Maria", year: 2021));
-        } 
-       
     }
 }
